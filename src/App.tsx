e5a0502d1bc4f15f280735af836769fb4ac1f1af
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2023-12-12 13:16:20 +0000

    Set up the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+
+  return (
+      <div>
+      <div className="tracker-content">
+        <h1 className="tracker-content__title">Ticket Tracker</h1>
+      </div>
+      </div>
+    
+  )
+}
+
+export default App
